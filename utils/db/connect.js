d709b3285959d2edb4afca3b7a4f20d5e207efd2
Numby86 <numby86@gmail.com>
2022-12-06 13:49:20 +0100

    (feat)uri,cloudinary and deploys api in vercel-finished

--- utils/db/connect.js
+++ utils/db/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const DB_URL = "mongodb+srv://root:7GBDSVHdMtJTfjZY@nodejs-proyectodb.ihshpoy.mongodb.net/?retryWrites=true&w=majority";
+const DB_URL = process.env.DB_URL;
 
 const connect = () => {
     mongoose.connect(DB_URL, {
